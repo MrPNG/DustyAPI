680e759bc6881bbdf350a6e199376d39a65429d2
Ian Szot <ian_marek@hotmail.com>
2018-01-21 13:44:48 -0200

    IP pinguim

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 // arquivo de configuração da API
 // IPs permitidos para fazer uma solicitação para a API
-$config['allowedIP'] = array("::1", "127.0.0.1", "localhost", "143.202.39.250", "189.54.216.246", "187.3.158.98", "187.3.205.35");
+$config['allowedIP'] = array("::1", "127.0.0.1", "localhost", "143.202.39.250", "189.54.216.246", "187.3.158.98", "187.3.205.35", "18.231.65.204");
 
 // Configuração da database
 $config['database']['ip'] = "alfa.desire.host";                  // IP da Database
